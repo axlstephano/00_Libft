f2b2538f47d1cc66116431bd625fdb36cceb6055
axcastil <axcastil@c2r14s4.42madrid.com>
2023-10-11 17:33:26 +0200

    111023

---
+++ ft_striteri.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 15:20:02 by axcastil          #+#    #+#             */
+/*   Updated: 2023/10/11 16:03:05 by axcastil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	s[i] = '\0';
+}
+
+//#include <stdio.h>
+//void printichar(unsigned int i, char *character)
+//{
+//    printf("Índice: %u, Carácter: %c\n", i, *character);
+//}
+//int main()
+//{
+//    char str[] = "hello";
+//    printf("Original: %s\n", str);
+//    ft_striteri(str, printichar);
+//    return (0);
+//}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 13:13:13 by axcastil          #+#    #+#             */
-/*   Updated: 2023/10/09 16:04:37 by axcastil         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:59:35 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
